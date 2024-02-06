ea09e6f3c73ab79d61eb94bdb553b5403265809d
NAZIR KHANUSHIYA <khanushiyainajir21@gnu.ac.in>
2024-02-06 12:57:46 +0530

    version 3

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello,This is code by Nazir Khanuhsiya to test the pipeline and this is version 2');
-  });
-  const port = process.env.PORT || 3000;
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(port, () => {
-  console.log(`API server listening on port ${port}`);
+  console.log("API server listening on port ${port}");
 });
