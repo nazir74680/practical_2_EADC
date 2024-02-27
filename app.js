b9beeba4fc64c3645a44cd7769c6838d0ad26ec9
NAZIR KHANUSHIYA <khanushiyainajir21@gnu.ac.in>
2024-02-27 20:53:13 +0530

    I have changed one line to  check Contionous delivery

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const app = express();
 const path = require('path');
 
 const port = process.env.PORT || 8080;
-
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
